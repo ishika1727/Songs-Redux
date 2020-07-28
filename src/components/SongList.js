2bab37f9a58196a3949e1cb5f7c6134d553e7684
ishika1727 <ishikadubey2000@gmail.com>
2020-07-29 01:04:00 +0530

    song detail rendered

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { selectSong } from '../actions';
 
 class SongList extends React.Component {
     renderList() {
@@ -7,7 +8,9 @@ class SongList extends React.Component {
             return (
                 <div className="item" key={song.title}>
                     <div className="right floated content">
-                        <button className="ui primary button">
+                        <button
+                            onClick={() => this.props.selectSong(song)}
+                            className="ui primary button">
                             Select
                         </button>
                     </div>
@@ -29,4 +32,6 @@ const mapStateToProps = (state) => {
     return { songs: state.songs };
 }
 
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, {
+    selectSong: selectSong
+})(SongList);
